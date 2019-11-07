fdd68228628ac4280b400e3cf8cccf671639c529
kuroyza <kuroyza@gmail.com>
2019-11-07 18:34:23 +0100

    Renamed Sql folder, added java folder and two new java solutions & added gitignore file

---
+++ 2. JAVA/A-Easy/Java_Loop_1.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Java_Loop_1{
+
+   private static final Scanner sc = new Scanner(System.in);
+
+   public static void main(String[] args){
+      int n = sc.nextInt();
+
+      sc.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+      int i;
+      for(i = 1;i <= 10;i++){
+         System.out.printf("%d x %d = %d\n",n,i,(n*i));
+      }
+
+      sc.close();
+   }
+}
---
+++ 2. JAVA/A-Easy/Java_Loop_2.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.Scanner;
+
+
+public class Java_Loop_2{
+   private static final Scanner sc = new Scanner(System.in);
+
+   public static int recursive(int sum, int b, int power, int n){
+      int currentSum = sum + (b * (int) Math.pow(2, power));
+
+      System.out.printf("%d ", currentSum);
+
+      return ++power < n ? recursive(currentSum, b, power, n) : 0;
+   }
+
+
+   public static void main(String[] args){
+      int t = sc.nextInt();
+
+      for(int i = 0; i < t; i++){
+         int a = sc.nextInt();
+         int b = sc.nextInt();
+         int n = sc.nextInt();
+         recursive(a, b, 0, n);
+         System.out.println();
+      }
+
+      sc.close();
+   }
+}
